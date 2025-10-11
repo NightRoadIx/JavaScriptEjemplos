33f858fe5cdc07ec2774036c93177cfbf7c74045
NightRoadIx <quintanarxbio@gmail.com>
2025-10-10 18:11:24 -0600

    Create ejemplo0001.js

---
+++ ejemplo0001.js
@@ -0,0 +1,8 @@
+// Estos son comentarios en JavaScript de una línea
+
+/*
+  Estos son comentarios de varias líneas
+*/
+
+// Para observar algo en la consola
+console.log("Esto es un mensaje en consola")
