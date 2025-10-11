90edb2577f0f95325c2a44f2b0ed22ed7d179dee
NightRoadIx <quintanarxbio@gmail.com>
2025-10-10 18:34:24 -0600

    Create ejemplo0006.js

---
+++ ejemplo0006.js
@@ -0,0 +1,11 @@
+// Arreglos se declaran de la siguiente forma:
+let nums = [2, 4, 6, 8];
+// Y pueden recorrerse usando su método forEach, el cual
+// es una especie de for que recorrera completo la colección
+// de datos
+// forEach(variable temporal => acción a realizar)
+nums.forEach(n => console.log(n * 2));
+
+// Operador ternario
+// variable = prueba lógica? valor verdadero : valor falso;
+const resultado = 2 > 3? 3 : 5;
