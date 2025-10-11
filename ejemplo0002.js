23f9a813f26c1d9a983bf983d1533703d316a8eb
NightRoadIx <quintanarxbio@gmail.com>
2025-10-10 18:16:23 -0600

    Create ejemplo0002.js

---
+++ ejemplo0002.js
@@ -0,0 +1,16 @@
+/*
+  Las variables en JavaScript pueden ser de 3 tipos:
+  var    - Funciona en el ámbito de la función, puede tener "hoisting" (elevación)
+  const  - Esta es una constante, no se puede reasignar
+  let    - Funciona en el ámbito de bloque
+*/
+let x = 10;
+let y = 5;
+// Proporciona directo en consola el resultado
+console.log("Suma: ", x + y);
+
+// JavaScript "lee" todo el código antes de ejecutarlo, por lo que es posible lo siguiente
+console.log(nombre); // El resultado es "undefined"
+var nombre = "Ana";
+// Esto, SOLO sucede con var, let y const no permiten esto
+// A esto se le llama "zona muerta temporal", el periodo entre la creación y asiganción de la variable
